da02f472c463b1ac12de4e68b0b95f8ec3be2121
Kampamba Shula <86658781+kshula@users.noreply.github.com>
2024-07-18 09:00:11 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,32 @@
+# List of provinces in Zambia
+provinces = [
+    "Central", "Copperbelt", "Eastern", "Luapula", "Lusaka", 
+    "Muchinga", "Northern", "North-Western", "Southern", "Western"
+]
+
+# Sample dictionary of district coordinates (latitude and longitude)
+district_coords = {
+    'KABWE': [-14.443, 28.445],
+    'KAPIRI MPOSHI': [-13.970, 28.665],
+    'NDOLA': [-12.970, 28.633],
+    'CHIENGE': [-9.542, 28.674],
+    'NCHELENGE': [-9.348, 28.733],
+    'CHONGWE': [-15.329, 28.697],
+    'LUSAKA': [-15.416, 28.283],
+    'MPULUNGU': [-8.764, 31.115],
+    'CHIBOMBO': [-14.655, 28.066],
+    'KITWE': [-12.821, 28.221],
+    'KAFUE': [-15.769, 28.182],
+    'SINAZONGWE': [-17.268, 27.461],
+    'MWENSE': [-10.384, 28.704],
+    'KAPUTA': [-8.467, 29.673],
+    'NSAMA': [-8.883, 30.281],
+    'LUANGWA': [-15.609, 30.387],
+    'MAZABUKA': [-15.858, 27.767]
+}
+
+# Coordinates for Kabwe and Broken Hill Mine
+kabwe_coords = [-14.444, 28.446]
+broken_hill_coords = [-14.4611,  28.4336]
+
+text = "Kabwe is one of the world's most polluted places due to extensive lead mining and smelting activities that took place at the Broken Hill Mine, also known as the Kabwe Mine. The mine, which operated from 1904 until 1994, has left a significant environmental and public health legacy."
